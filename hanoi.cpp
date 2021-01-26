b4577ee6f0f1d4ea64dcd301dd071f2898a87c8a
ZuzGom <74205927+ZuzGom@users.noreply.github.com>
2021-01-26 20:15:18 +0100

    Update hanoi.cpp

--- hanoi.cpp
+++ hanoi.cpp
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <iostream>
 #include <math.h>
 
@@ -14,65 +22,45 @@ int main()
  
     if (ile%2 == 1)
     {
+        while (x>0){
+        
+        if (x%4==0) cout<<"Wykonaj przeniesienie "<<b<<" - "<<a<<endl;
+        else cout<<"Wykonaj przeniesienie "<<a<<" - "<<b<<endl;
+        x--;
         p1 = b;
         b = c;
         c = p1;
+        if (x<1) break;
+        if (x%4==0) cout<<"Wykonaj przeniesienie "<<b<<" - "<<a<<endl;
+        else cout<<"Wykonaj przeniesienie "<<a<<" - "<<b<<endl;
+        x--;
+        
+        p1 = c;
+        c = a;
+        a = p1;
+        }
     }
-
-    while(x>=1){
+    else
+    {
+        while (x>0){
+        if (x%4==0) cout<<"Wykonaj przeniesienie "<<c<<" - "<<a<<endl;
+        else cout<<"Wykonaj przeniesienie "<<a<<" - "<<c<<endl;
+        x--;
+        p1 = b;
+        b = c;
+        c = p1;
+        
+        if (x<1) break;
+        if (x%4==0) cout<<"Wykonaj przeniesienie "<<c<<" - "<<a<<endl;
+        else cout<<"Wykonaj przeniesienie "<<a<<" - "<<c<<endl;
+        x--;
+        p1 = b;
+        b = a;
+        a = p1;
+        }
+    }
     
-        if(x<=0) break;
-        else {
-            cout<<"Wykonaj przeniesienie "<<a<<" - "<<c<<endl;
-            --x;
-        }
-        if(x<=0)  break;
-        else {
-            cout<<"Wykonaj przeniesienie "<< a<<" - "<<b<<endl;;
-            --x;
-                }
-        if(x<=0) break;
-        else {
-            cout<<"Wykonaj przeniesienie "<<c<<" - "<< b<<endl;
-            --x;
-            }
-            
-            
-            
-            
-        if(x<=0)  break;
-        else {
-            cout<<"Wykonaj przeniesienie "<<a<<" - "<< c<<endl;
-            --x;
-        }
-        
-        
-        
-            
-        if(x<=0)  break;
-        else {
-            cout<<"Wykonaj przeniesienie "<<b<<" - "<<a<<endl;
-            --x;
-        }
-        if(x<=0)  break;
-        else {
-            cout<<"Wykonaj przeniesienie "<< b<<" - "<<c<<endl;;
-            --x;
-                }
-        if(x<=0) break;
-        else {
-            cout<<"Wykonaj przeniesienie "<<a<<" - "<< c<<endl;
-            --x;
-            }
-            
-            
-            
-            
-        if(x<=0)  break;
-        else {
-            cout<<"Wykonaj przeniesienie "<<c<<" - "<< a<<endl;
-            --x;
-        }
-    }
+    
+    
     return 0;
 }
