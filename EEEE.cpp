fd7f785e1e5a07e2e0ba2ee854231b5ae4d00250
ZuzGom <74205927+ZuzGom@users.noreply.github.com>
2021-03-19 17:56:46 +0100

    Create EEEE.cpp

---
+++ EEEE.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    string liczba;
+    float mantysa;
+    int cecha, wgint[8];
+    cout<<"Podaj lizbe EEE156"<<endl;
+    cin>>liczba;
+    
+    
+    
+
+    return 0;
+}
