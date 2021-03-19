93427cc278f95f664a5a95eb3a014a0c9bd4f290
ZuzGom <74205927+ZuzGom@users.noreply.github.com>
2021-03-19 21:35:18 +0100

    Update EEEE.cpp

--- EEEE.cpp
+++ EEEE.cpp
@@ -1,10 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <iostream>
 #include <cmath>
@@ -14,13 +7,20 @@ using namespace std;
 int main()
 {
     string liczba;
-    float mantysa;
-    int cecha, wgint[8];
-    cout<<"Podaj lizbe EEE156"<<endl;
+    float mantysa=0,wgf[23];
+    int cecha=0, wgint[8], suma,b;
+    cout<<"Podaj liczbe IEEE754: "<<endl;
     cin>>liczba;
+    b = liczba[0]-'0';
     
+    for(int i=0;i<8;i++) wgint[i]=pow(2,7-i);
     
+    for(int i=0;i<4;i++) wgf[i]=pow(2,-i-1);
     
-
+    for (int i=1;i<9;i++) if (liczba[i]=='1') cecha += wgint[i-1];
+    
+    for (int i=9;i<24;i++) if (liczba[i]=='1') mantysa += wgf[i-9];
+    
+    cout<<pow(1,b)*(1+mantysa)*pow(2,cecha-127);
     return 0;
 }
