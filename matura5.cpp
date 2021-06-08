56899b4cc4b60176c71faf949b592d491174a768
ZuzGom <74205927+ZuzGom@users.noreply.github.com>
2021-06-08 18:49:31 +0200

    Update matura5.cpp

--- matura5.cpp
+++ matura5.cpp
@@ -1,29 +1,62 @@
 #include <bits/stdc++.h>
-#include <fstream>
-#include <string>
 using namespace std;
 
 string buildPalindrome(string st) {
-
 int e =st.length()-1,i=0;
-while (i<st.length()-1){
+while (e>=0){
     if (st[i]==st[e]) i++;
-    else e = st.length()-1;      
+    else i=0;      
     e--; 
 }
-if (st[st.length()-1]==st[st.length()-3]&&e>st.length()-3) e = st.length()-3;
-cout<<e;
-e--;
-while (e>=0){
-    st+=st[e];
-    e--;
+string wyn="";
+int j = st.length() -1;
+while (j>=i){
+    wyn+=st[j];
+    j--;
 }
+st=wyn+st;
 return st;
-
 }
+
 int main()
 {
-    cout<<buildPalindrome("kominiain");
-    
+    cout<<"5.1"<<endl;
+    int suma=0,ma=0,mi=0;
+    string maxi,mini;
+    fstream input;
+    input.open("slowa.txt");
+    while (!input.eof()){
+    string wyraz,wynik;
+    input>>wyraz;
+    wynik=buildPalindrome(wyraz);
+    int l= wynik.length();
+    if(mi==0)mi=l;
+    if (l==12) cout<<wynik<<endl;
+    suma+=l;
+    if (l>ma){
+        ma=l;
+        maxi=wynik;
+    }
+    if(l<mi){
+        mi=l;
+        mini=wynik;
+    }
+    }
+    cout<<"5.2"<<endl;
+    cout<<maxi<<endl;
+    cout<<mini<<endl;
+    cout<<"5.3"<<endl;
+    cout<<suma<<endl;
     return 0;
 }
+/*wynik
+5.1
+BDJBFBBFBJDB
+DFDAHEEHADFD
+IHBIBCCBIBHI
+FABBEAAEBBAF
+5.2
+AJBEIKKFIIGIIJBAHADIBCIDHDICBIDAHABJIIGIIFKKIEBJA
+OKOOKO
+5.3
+27751*/
